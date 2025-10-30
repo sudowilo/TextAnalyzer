8cf4ea412535a3002e086b0f6145e519edf648c0
wilo <sudowilo@gmail.com>
2025-10-30 23:17:54 +0330

    Reading files from text file

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿class Program
+{
+  static void Main(string[] args)
+  {
+    if (args.Length == 0)
+    {
+      Console.WriteLine("Specify a file path");
+      return;
+    }
+    string path = args[0];
+    if (!File.Exists(path))
+    {
+      Console.WriteLine("There is no such file.");
+      return;
+    }
+
+    StreamReader reader = new StreamReader(path);
+
+    int linesCount = 0;
+    string? line;
+    while ((line = reader.ReadLine()) != null)
+    {
+      Console.WriteLine(++linesCount + "-" + line);
+    }
+  }
+}
