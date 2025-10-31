19fc288a98a8ce92357d59db222ef02fd2cdd145
wilo <sudowilo@gmail.com>
2025-10-31 18:54:48 +0330

    Finding top5 frequent words

--- Program.cs
+++ Program.cs
@@ -3,6 +3,21 @@ using System.Reflection.Metadata;
 
 class Program
 {
+  private static void addWordToDictionary(Dictionary<string, int> dictionary, string dictionaryWord)
+  {
+    if (dictionaryWord.Length > 0)
+    {
+      if (dictionary.ContainsKey(dictionaryWord))
+      {
+        dictionary[dictionaryWord] += 1;
+      }
+      else
+      {
+        dictionary[dictionaryWord] = 1;
+      }
+    }
+  }
+
   static void Main(string[] args)
   {
     if (args.Length == 0)
@@ -21,10 +36,10 @@ class Program
     int linesCount = 0;
     int nonSpaceCharactersCount = 0;
     int charactersCount = 0;
-    int wordsCount = 0;
+    string word = "";
+    Dictionary<string, int> words = new Dictionary<string, int>();
 
     bool previousCharSpaceValue = false;
-    bool lineHasValue = false;
 
     string? line;
     while ((line = reader.ReadLine()) != null)
@@ -33,30 +48,51 @@ class Program
 
       foreach (var character in line)
       {
-        lineHasValue = true; 
-        if (character == ' ')
+        if (!((character >= 'A' && character <= 'Z') || (character >= 'a' && character <= 'z')) && character != '#' && character != '@')
         {
-          if (!previousCharSpaceValue)
-          {
-            previousCharSpaceValue = true;
-            ++wordsCount;
-          }
+          if (!previousCharSpaceValue) previousCharSpaceValue = true;
+
+          if (word.Length > 1) addWordToDictionary(words, word.ToLower());
+          word = ""; //flash the contents
         }
         else
         {
           previousCharSpaceValue = false;
           ++nonSpaceCharactersCount;
+          word += character;
         }
       }
 
-      if (lineHasValue) ++wordsCount;  //In this way we can add the last word on the line (if there is any word).
       ++linesCount;
-      lineHasValue = false;
+      addWordToDictionary(words, word);
+      word = ""; //flash the contents
     }
+
+    int wordsNumber = 0;
+    foreach (var pair in words)
+    {
+      wordsNumber += pair.Value;
+    }
+
+    var sorted = words
+    .OrderByDescending(kvp => kvp.Value)
+    .ToList();
+
 
     Console.WriteLine("number of lines: " + linesCount);
     Console.WriteLine("number of characters: " + (charactersCount + linesCount - 1)); //For \n characters
     Console.WriteLine("number of non-space characters: " + nonSpaceCharactersCount);
-    Console.WriteLine("number of words: " + wordsCount);
+    Console.WriteLine("number of words: " + wordsNumber);
+    Console.WriteLine("number of words base on Dictionary: " + words.Count);
+
+    Console.WriteLine("Top 5 frequent words: ");
+    var count = 0;
+    foreach (var pair in sorted)
+    {
+      Console.WriteLine($"{pair.Key}: {pair.Value}");
+      count++;
+      if (count >= 5) break;
+    }
+
   }
 }
